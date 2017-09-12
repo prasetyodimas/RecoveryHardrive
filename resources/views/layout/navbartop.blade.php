3eeecc888a25764e077c9bf1e5fbe20f735841f4
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2017-09-12 19:02:04 +0700

    add new theme and design

--- resources/views/layout/navbartop.blade.php
+++ resources/views/layout/navbartop.blade.php
@@ -14,7 +14,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{url('/')}}">Home</a></li> 
+                <li class="{{Request::segment(1)}}"><a href="{{url('/')}}">Home</a></li> 
                 <li><a href="{{url('about')}}">About</a></li> 
                 <li><a href="{{url('service')}}">Services</a></li> 
                 <li><a href="{{url('contact')}}">Contact</a></li> 
