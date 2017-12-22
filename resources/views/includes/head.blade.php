618ebcbff159f2f14830d75eed1a5117beed45a9
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2017-12-22 22:53:24 +0700

    add new changhing template

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,11 +1,13 @@
 <title>{{Theme::titleApps()}}</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta keyword="Jasa Recovery Hardisk, Jasa Recovery Data, Recovery Data Jogja, Recovery Terbaik Jogja, Recovery Hardisk Jogja dan Perbaikan Komputer, Terima Segala Macam Jenis Keluhan Komputer, Solusi Terpercaya Terjamin">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="description" content="Jasa Recovery Hardisk, Jasa Recovery Data, Recovery Data Jogja, Recovery Terbaik Jogja, Recovery Hardisk Jogja dan Perbaikan Komputer, Terima Segala Macam Jenis Keluhan Komputer, Solusi Terpercaya Terjamin">
-<meta name="author" content="Dimas Prasetyo">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Description" content="Jasa Recovery Hardisk dan Jasa Recovery Data, Recovery Data Jogja, Recovery Terbaik Jogja, Recovery Hardisk Jogja dan Perbaikan Komputer, Terima Segala Macam Jenis Keluhan Komputer, Solusi Terpercaya Terjamin">
+<meta name="Author" content="Dimas Prasetyo">
+<meta keyword="Jasa Recovery Hardisk, Jasa Recovery Data, Recovery Data Jogja, Recovery Terbaik Jogja, Recovery Hardisk Jogja dan Perbaikan Komputer, Terima Segala Macam Jenis Keluhan Komputer, Solusi Terpercaya Terjamin">
+<link rel="Publisher" href="https://plus.google.com/u/0/107759764226131214282">
+<link rel="Canonical" href="http://dimasrecovery.com/">
 <!-- FAVICON -->
 <link rel="icon" type="image/png" sizes="16x16" href="{{asset('frontend/fronthemes/asset-images/favico/favicon-16.png')}}">
 <!-- CSS -->
